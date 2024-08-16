8b47f300b412f7455e4bae655c310de5a52b5988
Mykhailo <97468374+UlanovychM@users.noreply.github.com>
2024-08-16 11:49:55 +0300

    start repiting practice js

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const restartBtn = document.querySelector('.again');
+const inputNumber = document.querySelector('.number-input');
+const checkerBtn = document.querySelector('.check');
+const takeScore = document.querySelector('.label-score');
+const highScore = document.querySelector('.highscore');
