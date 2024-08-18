e0b1d1b31bbfbcc7a2ce4dde80bdf614886ad8b5
Mykhailo Ulanovych <97468374+UlanovychM@users.noreply.github.com>
2024-08-18 21:49:13 +0300

    update

--- script.js
+++ script.js
@@ -1,39 +1,59 @@
 'use strict';
 
 const restartBtn = document.querySelector('.again');
-const form = document.querySelector('.left');
+const inputNumber = document.querySelector('.number-input');
 const checkerBtn = document.querySelector('.check');
 const takeScore = document.querySelector('.label-score');
-const highScore = document.querySelector('.highscore');
-const riddleNumber = 13;
+const highScore = document.querySelector('.label-highscore');
+const textMessage = document.querySelector('.guess-message');
+const riddleNumber = 12;
+let startNumber = 20;
 
 restartBtn.addEventListener('click', () => {
-  document.querySelector('main').innerHTML = `
-	<form class="left">
+  document.querySelector('body').innerHTML = `
+   <header>
+      <h1>Угадай Число!</h1>
+      <p class="between">(Между 1 и 20)</p>
+      <button class="btn again">Сначала!</button>
+      <div class="question">???</div>
+    </header>
+	<main>
+      <div class="left">
         <input type="number" class="number-input" />
         <button class="btn check">Проверить!</button>
-      </form>
+      </div>
       <section class="right">
         <p class="guess-message">Начни угадывать</p>
         <p class="label-score">Очки: <span class="score">20</span></p>
         <p class="label-highscore">
           Лучший результат: <span class="highscore">0</span>
         </p>
-  </section>`;
+      </section>
+    </main>`;
 });
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  const inputNumber = document.querySelector('[type="number"]');
-  const question = document.querySelector('.question');
+const minesNumber = () => {
+  takeScore.innerHTML = `Очки: <span class="score">${(startNumber -= 1)}</span>`;
+};
 
-  const formValue = {
+checkerBtn.addEventListener('click', () => {
+  const inputValue = {
     number: inputNumber.value,
   };
+  let sum;
 
-  if (formValue.number === riddleNumber) {
-    question.innerHTML = `<div class="question">${riddleNumber}</div>`;
+  if (inputValue.number > riddleNumber) {
+    textMessage.textContent = 'Забагато';
+    minesNumber();
+  } else if (inputValue.number < riddleNumber) {
+    textMessage.textContent = 'Замало';
+    minesNumber();
+  } else if (inputValue.number == riddleNumber) {
+    document.querySelector(
+      '.question'
+    ).innerHTML = `<div class="question">${inputValue.number}</div>`;
+    takeScore.innerHTML = `Очки: <span class="score">${(startNumber += 1)}</span>`;
+    highScore.innerHTML = `<span class="highscore">${sum}</span>`;
+    textMessage.textContent = 'Правильно';
   }
-
-  // form.reset();
 });
